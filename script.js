06272cd3172a52184135c8a4bcdc75f9454f9634
Bearded_Elino <elijahonuh2@gmail.com>
2024-01-17 09:39:25 +0100

    a simple calculator app

---
+++ script.js
@@ -0,0 +1,71 @@
+/* (function() {
+
+
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let equal = document.querySelector('.btn-equal');
+    let clear = document.querySelector('.btn-clear')
+    
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    })
+
+    
+
+
+    equal.addEventListener('click', function(e) {
+        if (screen.value === '') {
+            screen.value = "please enter";
+        }
+
+        else {
+          let answer = eval(screen.value)
+          screen.value = answer;
+          console.log("answer")
+        }
+    })
+
+    clear.addEventListener ('click', function (e) {
+        screen.value = "";
+    })
+
+
+
+
+
+})() */
+
+
+(function() {
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.set;
+            screen.value += value;
+        })
+    })
+
+    equal.addEventListener('click', function(e) {
+        if (screen.value === ''){
+            screen.value ="enter a value"
+        }
+
+        else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+            // console.log("answer");
+        }
+    })
+
+    clear.addEventListener('click', function(e) {
+        screen.value = "";
+    })
+})()
